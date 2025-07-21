63d2fa2f7f7e06bd300a7cae95a81a181fe6c254
Ishay Yemini <ishay.yemini@gmail.com>
2025-07-21 14:06:50 +0300

    Result shows up in top-5

--- attack.py
+++ attack.py
@@ -18,11 +18,13 @@ class BlackBoxAttack:
         # The query we optimize for
         self.q_emb = self.model.encode(q, convert_to_tensor=True)
         # The corpus
-        self.corpus = corpus
+        self.corpus = np.array(corpus)
+        # Embedding of corpus
+        self.corpus_emb = [self.model.encode(c, convert_to_tensor=True) for c in corpus]
         # Amount of tokens to append to p_adv
-        self.num_tokens = 20
+        self.num_tokens = num_tokens
         # Amount of tokens to draw on each round
-        self.num_pool = 500
+        self.num_pool = num_pool
         # Compute the embedding of each token and store the result in a matrix E
         self.tokenizer = model.tokenizer
         self.cos_sim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
@@ -53,3 +55,17 @@ class BlackBoxAttack:
         print(f"similarity with tokens: {curr_best}")
 
         return tokens
+
+    def add_to_corpus(self, c: str):
+        corpus = self.corpus.tolist()
+        corpus.append(c)
+        self.corpus = np.array(corpus)
+        self.corpus_emb.append(self.model.encode(c, convert_to_tensor=True))
+
+    def get_top_k(self, q: str, k=5):
+        q_emb = self.model.encode(q, convert_to_tensor=True)
+        topk = torch.topk(
+            torch.tensor([self.cos_sim(q_emb, c_emb) for c_emb in self.corpus_emb]), k=k
+        )
+        print(topk)
+        return self.corpus[topk.indices]
