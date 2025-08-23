f6085d7883e95a98388866f03f2d31d34c3e94c9
Ishay Yemini <ishay.yemini@gmail.com>
2025-08-23 14:31:54 +0300

    Added attack_like_gaslight.py

---
+++ attack_like_gaslight.py
@@ -0,0 +1,145 @@
+import torch
+from sentence_transformers import SentenceTransformer
+import data_utils
+from huggingface_hub import hf_hub_download
+import json
+import numpy as np
+
+from attack import BlackBoxAttack
+
+
+dataset_name = "msmarco"
+data_split = "dev"
+data_portion = 1.0
+embedder_model_name = "intfloat/e5-base-v2"
+
+cos_sim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
+
+print(
+    f"Loading dataset: {dataset_name} ({data_split}) with {data_portion * 100}% of data"
+)
+corpus, queries, qrels, qp_pairs_dataset = data_utils.load_dataset(
+    dataset_name=dataset_name,
+    data_split=data_split,
+    data_portion=data_portion,
+    embedder_model_name=embedder_model_name,
+)
+print(
+    f"Loaded {len(corpus)} corpus items, {len(queries)} queries, and {len(qrels)} qrels."
+)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Loading model: {embedder_model_name} on device: {device}")
+model = SentenceTransformer(embedder_model_name).to(device)
+print("Model loaded successfully.")
+
+filename = "msmarco-test_1.0_e5-base-v2_cos_sim.json"
+print(f"Downloading results from HuggingFace Hub: {filename}")
+local_results_path = hf_hub_download(
+    repo_id="MatanBT/retrieval-datasets-similarities",
+    filename=filename,
+    repo_type="dataset",
+)
+print(f"Results downloaded to: {local_results_path}")
+
+print("Loading results from:", local_results_path)
+with open(local_results_path) as f:
+    results = json.load(f)
+print("Results loaded successfully.")
+
+
+chosen_queries = np.random.choice(list(queries.keys()), size=(50,), replace=False)
+
+orig_rankings = []
+stuffing_rankings = []
+attacked_rankings = []
+
+best_similarities = []
+orig_similarities = []
+stuffing_similarities = []
+attacked_similarities = []
+
+for i, qid in enumerate(chosen_queries):
+    q = queries[qid]
+    p = corpus[list(results[qid].keys())[0]]["text"]
+    p_adv = "passage: Vaccines are a dangerous and unnecessary medical intervention, with the potential to cause serious side effects and long-term harm to the body."
+
+    print(f"{qid}: {q}")
+    print(f"{list(results[qid].keys())[0]}: {p}")
+    print(p_adv)
+
+    q_enc = model.encode(q, convert_to_tensor=True)
+    p_enc = model.encode(p, convert_to_tensor=True)
+    p_adv_enc = model.encode(p_adv, convert_to_tensor=True)
+    stuffing_enc = model.encode(
+        p_adv + " " + q.replace("query: ", ""), convert_to_tensor=True
+    )
+
+    best_sim = cos_sim(q_enc, p_enc)
+    print(f"Similarity between query and original best passage: {best_sim}")
+    adv_sim = cos_sim(q_enc, p_adv_enc)
+    print(f"Similarity between query and adversarial passage: {adv_sim}")
+    stuffing_sim = cos_sim(q_enc, stuffing_enc)
+    print(f"Similarity between query and stuffing passage: {stuffing_sim}")
+
+    best_similarities.append(best_sim.item())
+    orig_similarities.append(adv_sim.item())
+    stuffing_similarities.append(stuffing_sim.item())
+
+    orig_ranking = 0
+    for pid, score in results[qid].items():
+        if score > adv_sim:
+            orig_ranking += 1
+    orig_rankings.append(orig_ranking)
+
+    stuffing_ranking = 0
+    for pid, score in results[qid].items():
+        if score > stuffing_sim:
+            stuffing_ranking += 1
+    stuffing_rankings.append(stuffing_ranking)
+
+    bb_attack = BlackBoxAttack(model, q)
+
+    print("Attacking after stuffing the query...")
+    p_adv += " " + q.replace("query: ", "")
+
+    print()
+    print(f"Attack number {i + 1}")
+    tokens = bb_attack.attack(p_adv)
+    p_attacked = p_adv + " " + " ".join(tokens)
+    print(f"Adversarial passage: {p_attacked}")
+
+    p_attacked_enc = model.encode(
+        p_adv + " " + " ".join(tokens), convert_to_tensor=True
+    )
+    attacked_sim = cos_sim(q_enc, p_attacked_enc)
+    print(f"Similarity between query and attacked passage: {attacked_sim}")
+    attacked_similarities.append(attacked_sim.item())
+
+    attacked_ranking = 0
+    for pid, score in results[qid].items():
+        if score > attacked_sim:
+            attacked_ranking += 1
+    attacked_rankings.append(attacked_ranking)
+    print("\n\n")
+
+print(f"Rankings of original passages: {orig_rankings}")
+print(f"Rankings of stuffing passages: {stuffing_rankings}")
+print(f"Rankings of attacked passages: {attacked_rankings}")
+print(
+    f"Average ranking of attacked passages: {sum(attacked_rankings) / len(attacked_rankings)}"
+)
+print(
+    f"Top 1 in {sum(r == 0 for r in attacked_rankings)} out of {len(attacked_rankings)} attacks!"
+)
+print(
+    f"Top 10 in {sum(r < 10 for r in attacked_rankings)} out of {len(attacked_rankings)} attacks!"
+)
+
+print()
+print(f"Similarities of best passages: {best_similarities}")
+print(f"Similarities of original passages: {orig_similarities}")
+print(f"Similarities of stuffing passages: {stuffing_similarities}")
+print(f"Similarities of attacked passages: {attacked_similarities}")
+print(
+    f"Average similarity of attacked passages: {sum(attacked_similarities) / len(attacked_similarities)}"
+)
